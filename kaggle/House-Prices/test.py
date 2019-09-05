a164f62b2f25d7fa2c7f90755bd34d32da85dde3
helong <nevermore.he@aispeech.com>
2019-09-05 15:54:49 +0800

    toolbox:add demo.py

---
+++ kaggle/House-Prices/test.py
@@ -0,0 +1,6 @@
+import os,sys
+
+sys.path.append('../../')
+from toolbox.test import A
+
+A(1,2).add()
---
+++ toolbox/test.py
@@ -0,0 +1,6 @@
+class A(object):
+    def __init__(self,xx,yy):
+        self.x=xx
+        self.y=yy
+    def add(self):
+        print("sum(x,y):%d"%(self.x+self.y))
